e577e3164d81c859256194aea775a33c5db834c9
anybub <anubhabbose469@gmail.com>
2024-07-18 23:46:55 +0530

    change in backend

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: "https://restaurant-mauve-phi.vercel.app/",
     methods: ["POST"],
     credentials: true,
   })
