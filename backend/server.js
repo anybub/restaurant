cb34fb522bf15f1ea79be6b12f83a167dc2d30b8
anybub <anubhabbose469@gmail.com>
2024-07-18 23:12:24 +0530

    change in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+app.listen(8080, ()=>{
+    console.log("SERVER HAS STARTED AT PORT 8080");
 })
