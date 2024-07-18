48c4afc77ab8a111437590c7895bef7b3c1b6de9
anybub <anubhabbose469@gmail.com>
2024-07-18 23:36:19 +0530

    change in frontend url handling

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/reservation/send",
+        "https://restaurant-h2g2.onrender.com/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
